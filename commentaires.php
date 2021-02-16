7832992eb2678dce9b27cf01b9f6c351f6c69b3d
jocelyn DUQUESNOY <jocelyn.duquesnoy@yahoo.fr>
2021-02-16 18:11:45 +0100

    ajout du php et du html pour la page acteur /commentaires

--- commentaires.php
+++ commentaires.php
@@ -5,83 +5,81 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
+  <link href="css/all.css" rel="stylesheet"> <!--load all styles -->
   <title>commentaires blog</title>
 </head>
-<body>
-<h1>Mon super blog!</h1>
-<p><a href="index.php">Retour à la liste des billets</a></p>
 <?php
 try
 {
 	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=extranet_bancaire;charset=utf8', 'root', 'root');
 }
 catch(Exception $e)
 {
 	// En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : '.$e->getMessage());
 }
-
-// Si tout va bien, on peut continuer
-
-// On récupère tout le contenu de la table jeux_video
-// $reponse = $bdd->query('SELECT * FROM billets ORDER BY date_creation DESC LIMIT 5');
-$reponse = $bdd->prepare("SELECT titre, contenu, id, date_creation, DAY(date_creation) AS jour, MONTH(date_creation) AS mois,
- YEAR(date_creation) AS annee, HOUR(date_creation) AS heure, MINUTE(date_creation) AS minute, SECOND(date_creation) AS seconde FROM billets WHERE id=?");
-$reponse->execute(array($_GET['id']));
-// On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{
-?>
-<div class="news">
-  <h3>
-    <?php echo htmlspecialchars($donnees['titre']);?> <em>le <?php echo $donnees['jour'];?>/<?php echo $donnees['mois'];?>/<?php echo $donnees['annee'];?> à <?php echo $donnees['heure'];?>h<?php echo $donnees['minute'];?>m<?php echo $donnees['seconde'];?>s</em>
-  </h3>
-  <p>
-    <?php echo nl2br(htmlspecialchars($donnees['contenu']));?><br/>
-  </p>
-</div>
- 
-<?php
-}
-
-$reponse->closeCursor(); // Termine le traitement de la requête
-
-?>
-<h2>Commentaires</h2>
-<?php
-try
-{
-	// On se connecte à MySQL
-	$bdd_a = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
-
-// Si tout va bien, on peut continuer
-
-// On récupère tout le contenu de la table jeux_video
-// $reponse = $bdd->query('SELECT * FROM billets ORDER BY date_creation DESC LIMIT 5');
-$reponse_a = $bdd_a->prepare("SELECT id_billet, auteur, id, date_commentaire,commentaire, DAY(date_commentaire) AS jour, MONTH(date_commentaire) AS mois,
- YEAR(date_commentaire) AS annee, HOUR(date_commentaire) AS heure, MINUTE(date_commentaire) AS minute, SECOND(date_commentaire) AS seconde FROM commentaires WHERE id_billet=?");
+$reponse_a = $bdd->prepare("SELECT * FROM acteurs WHERE id_acteur=?");
 $reponse_a->execute(array($_GET['id']));
-// On affiche chaque entrée une à une
 while ($donnees_a = $reponse_a->fetch())
 {
+
+?>
+<div class="news">
+  <p>
+    <img src="logos/<?php echo htmlspecialchars($donnees_a['logo']); ?>" alt="logo acteur">
+  </p>
+  <h3>
+    <?php echo htmlspecialchars($donnees_a['description']);?>
+    <a href="<?php echo $donnees_a['lien'];?>"><?php echo $donnees_a['lien'];?></a>
+  </h3>
+  <p>
+    <button  type="button"><a href="commentaires.php?id=<?php echo $donnees_a['id']; ?>">Lire la suite</a></button>
+  </p>
+</div>
+<?php
+}
+$reponse_a->closeCursor(); // Termine le traitement de la requête
 ?>
 
+<h2>Commentaires</h2> <button>nouveau commentaire</button><a href="like">like</a><i class="far fa-thumbs-up"></i>
+<?php
+
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+// $reponse = $bdd->query('SELECT * FROM billets ORDER BY date_creation DESC LIMIT 5');
+$reponse_b = $bdd->prepare("SELECT id_post, id_user, id_acteur, date_add,post, DAY(date_add) AS jour, MONTH(date_add) AS mois,
+ YEAR(date_add) AS annee, HOUR(date_add) AS heure, MINUTE(date_add) AS minute, SECOND(date_add) AS seconde FROM posts WHERE id_acteur=?");
+$reponse_b->execute(array($_GET['id']));
+// On affiche chaque entrée une à une
+while ($donnees_b = $reponse_b->fetch())
+{
+?>
+  <?php
+  $reponse_c = $bdd->prepare("SELECT * FROM accounts WHERE id_user=?");
+  $reponse_c->execute(array($donnees_b['id_user']));
+  // On affiche chaque entrée une à une
+  while ($donnees_c = $reponse_c->fetch())
+  {
+  ?>
+  
   <p>
-  <strong><?php echo htmlspecialchars($donnees_a['auteur']);?></strong> le <?php echo $donnees_a['jour'];?>/<?php echo $donnees_a['mois'];?>/<?php echo $donnees_a['annee'];?> à <?php echo $donnees_a['heure'];?>h<?php echo $donnees_a['minute'];?>m<?php echo $donnees_a['seconde'];?>s</br>
-  <?php echo nl2br(htmlspecialchars($donnees_a['commentaire']));?>
+  <strong><?php echo htmlspecialchars($donnees_c['prenom']);?>
+  </p>
+  <?php    
+  }
+  $reponse_c->closeCursor();
+  ?>
+
+  <p>
+    </strong> le <?php echo $donnees_b['jour'];?>/<?php echo $donnees_b['mois'];?>/<?php echo $donnees_b['annee'];?> à <?php echo $donnees_b['heure'];?>h<?php echo $donnees_b['minute'];?>m<?php echo $donnees_b['seconde'];?>s</br>
+    <?php echo nl2br(htmlspecialchars($donnees_b['post']));?>
   </p>
 <?php
 }
-
-$reponse_a->closeCursor(); // Termine le traitement de la requête
-
+$reponse_b->closeCursor(); // Termine le traitement de la requête
 ?>
 </body>
 </html>
