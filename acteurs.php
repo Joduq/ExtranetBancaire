9493e5d400c0cc043e2f8e5c664e9904babd77b1
jocelyn DUQUESNOY <jocelyn.duquesnoy@yahoo.fr>
2021-02-23 09:59:48 +0100

    style

--- acteurs.php
+++ acteurs.php
@@ -4,11 +4,9 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="style.css" rel="stylesheet" >
-  <link href="css/all.css" rel="stylesheet">
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" href="style.css" />
   <title>blog</title>
 </head>
 <body>
